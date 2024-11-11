f5600b5c3a6820836e04c292ae94713c9803701f
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-11 22:20:14 +0530

    x mark button view

---
+++ SwiftfulCrypto/Core/Components/XMarkButton.swift
@@ -0,0 +1,28 @@
+//
+//  XMarkButton.swift
+//  SwiftfulCrypto
+//
+//  Created by I O N Groups Pvt Ltd on 2024-10-23.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        })
+    }
+}
+
+struct XMarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XMarkButton()
+    }
+}
