ac92af67c09830e24f907b8332c5e6bebccda390
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-13 20:00:49 +0530

    settings view

---
+++ SwiftfulCrypto/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsView.swift
+//  SwiftfulCrypto
+//
+//  Created by I O N Groups Pvt Ltd on 2024-10-27.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+   
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
